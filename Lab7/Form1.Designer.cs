f0c8643e80877b5c059bd052574d6ddbfedcbdbe
RuLe0G <khlebnikov2015@yandex.ru>
2020-12-26 17:47:33 +0700

    Initial commit

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(16, 15);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(853, 591);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(985, 13);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(853, 591);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(877, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(877, 49);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(877, 85);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(62, 325);
+            this.imageBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(1707, 591);
+            this.imageBox3.TabIndex = 22;
+            this.imageBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(928, 13);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 28);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(879, 122);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1848, 1055);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.Drawing.Imaging;
+using System.Drawing;
+using Emgu.CV.OCR;
+using Emgu.CV.Features2D;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Image<Bgr, byte> baseImage; //глобальная переменная
+        public Image<Bgr, byte> twistedImage; //глобальная переменная
+
+        //private UCDLRAOCI func = new UCDLRAOCI();
+
+        public Form1()
+        {
+            InitializeComponent();
+            imageBox3.Visible = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                baseImage = new Image<Bgr, byte>(fileName);
+
+                imageBox1.Image = baseImage.Resize(640, 480, Inter.Linear);
+            }
+
+            
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла            
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                twistedImage = new Image<Bgr, byte>(fileName);
+
+                imageBox2.Image = twistedImage.Resize(640, 480, Inter.Linear);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {             
+            GFTTDetector detector = new GFTTDetector(40, 0.01, 5, 3, true);
+            MKeyPoint[] GFP1 = detector.Detect(baseImage.Convert<Gray, byte>().Mat);
+
+            //создание массива характерных точек исходного изображения (только позиции)
+            PointF[] srcPoints = new PointF[GFP1.Length];
+            for (int i = 0; i < GFP1.Length; i++)
+                srcPoints[i] = GFP1[i].Point;
+
+            PointF[] destPoints; //массив для хранения позиций точек на изменённом изображении
+            byte[] status; //статус точек (найдены/не найдены)
+            float[] trackErrors; //ошибки
+                                 //вычисление позиций характерных точек на новом изображении методом Лукаса-Канаде
+
+            CvInvoke.CalcOpticalFlowPyrLK(
+             baseImage.Convert<Gray, byte>().Mat, //исходное изображение
+             twistedImage.Convert<Gray, byte>().Mat,//изменённое изображение
+             srcPoints, //массив характерных точек исходного изображения
+             new Size(20, 20), //размер окна поиска
+             5, //уровни пирамиды
+             new MCvTermCriteria(20, 1), //условие остановки вычисления оптического потока
+             out destPoints, //позиции характерных точек на новом изображении
+             out status, //содержит 1 в элементах, для которых поток был найден
+             out trackErrors //содержит ошибки
+             );
+
+            //вычисление матрицы гомографии
+            Mat homographyMatrix = CvInvoke.FindHomography(destPoints, srcPoints, RobustEstimationAlgorithm.LMEDS);
+            var destImage = new Image<Bgr, byte>(baseImage.Size);
+            CvInvoke.WarpPerspective(twistedImage, destImage, homographyMatrix, destImage.Size);
+
+
+            
+
+            var output = baseImage.Clone();
+
+            foreach (PointF p in srcPoints)
+            {
+                CvInvoke.Circle(output, Point.Round(p), 3, new Bgr(Color.Blue).MCvScalar, 5);
+            }
+            imageBox1.Image = output.Resize(640, 480, Inter.Linear);
+
+            var output2 = destImage.Clone();
+
+            foreach (PointF p in destPoints)
+            {
+                CvInvoke.Circle(output2, Point.Round(p), 3, new Bgr(Color.Blue).MCvScalar, 5);
+            }
+
+            imageBox2.Image = output2.Resize(640, 480, Inter.Linear);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {            
+            GFTTDetector detector = new GFTTDetector(40, 0.01, 5, 3, true);
+
+            var baseImgGray = baseImage.Convert<Gray, byte>();
+            var twistedImgGray = twistedImage.Convert<Gray, byte>();
+
+            //генератор описания ключевых точек
+            Brisk descriptor = new Brisk();
+
+            //поскольку в данном случае необходимо посчитать обратное преобразование
+            //базой будет являться изменённое изображение
+            VectorOfKeyPoint GFP1 = new VectorOfKeyPoint();
+            UMat baseDesc = new UMat();
+            UMat bimg = twistedImgGray.Mat.GetUMat(AccessType.Read);
+
+            VectorOfKeyPoint GFP2 = new VectorOfKeyPoint();
+            UMat twistedDesc = new UMat();
+            UMat timg = baseImgGray.Mat.GetUMat(AccessType.Read);
+
+            //получение необработанной информации о характерных точках изображений
+            detector.DetectRaw(bimg, GFP1);
+            //генерация описания характерных точек изображений
+            descriptor.Compute(bimg, GFP1, baseDesc);
+
+            detector.DetectRaw(timg, GFP2);
+            descriptor.Compute(timg, GFP2, twistedDesc);
+
+            //класс позволяющий сравнивать описания наборов ключевых точек
+            BFMatcher matcher = new BFMatcher(DistanceType.L2);
+
+            //массив для хранения совпадений характерных точек
+            VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch();
+            //добавление описания базовых точек
+            matcher.Add(baseDesc);
+            //сравнение с описанием изменённых
+            matcher.KnnMatch(twistedDesc, matches, 2, null);
+            //3й параметр - количество ближайших соседей среди которых осуществляется поиск совпадений
+            //4й параметр - маска, в данном случае не нужна
+
+            //маска для определения отбрасываемых значений (аномальных и не уникальных)
+            Mat mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+            mask.SetTo(new MCvScalar(255));
+            //определение уникальных совпадений
+            Features2DToolbox.VoteForUniqueness(matches, 0.8, mask);
+
+            var res = new Image<Bgr, byte>(baseImage.Size);
+            //отбрасывание совпадения, чьи параметры масштабирования и поворота не совпадают с параметрами большинства
+            int nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(GFP1, GFP1, matches, mask, 1.5, 20);
+            Features2DToolbox.DrawMatches(twistedImage, GFP1, baseImage, GFP2, matches, res, new MCvScalar(255, 0, 0), new MCvScalar(255, 0, 0), mask);
+
+            imageBox3.Image = res.Resize(1280, 480, Inter.Linear);
+
+            Mat homography;
+            //получение матрицы гомографии
+            homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(GFP1, GFP2, matches, mask, 2);
+
+            var destImage = new Image<Bgr, byte>(baseImage.Size);
+            CvInvoke.WarpPerspective(twistedImage, destImage, homography, destImage.Size);
+
+            imageBox2.Image = destImage.Resize(640, 480, Inter.Linear);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                imageBox3.Visible = true;
+            }
+            else
+            {
+                imageBox3.Visible = false;
+            }
+        }
+    }
+}
